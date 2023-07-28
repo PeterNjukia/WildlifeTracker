208a6704d7b40eeaa79be8b4ffd90672db684dc2
unknown <pnjukia@gmail.com>
2023-07-28 16:51:38 +0300

    main.java file updates

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.sql.Timestamp;
 
-//abcdefghijklm
+//abcdefghijklmn
 public class Animal {
     public int id;
     public String name;
